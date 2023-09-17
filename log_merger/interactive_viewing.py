92666faeac94b45fc18e7ce92d22129813926040
ptmcg <ptmcg@austin.rr.com>
2023-09-16 22:31:59 -0500

    Add ModalInputDialog widget for popup query; implement "Go to line" UI action

--- log_merger/interactive_viewing.py
+++ log_merger/interactive_viewing.py
@@ -1,12 +1,116 @@
-from datetime import datetime
 import textwrap
 import types
 
 import littletable as lt
 from rich.text import Text
+from textual import on
 from textual.app import App, ComposeResult
 from textual.binding import Binding
-from textual.widgets import DataTable, Footer
+from textual.containers import Horizontal, Vertical
+from textual.screen import ModalScreen
+from textual.validation import Validator, Integer, Function
+from textual.widgets import Button, DataTable, Footer, Input, Label
+
+
+class ModalInputDialog(ModalScreen[str]):
+    """
+    A modal dialog for getting a single input from the user.
+    (cribbed from https://github.com/Textualize/frogmouth/blob/main/frogmouth/dialogs/input_dialog.py)
+    """
+
+    DEFAULT_CSS = """
+    InputDialog {
+        align: center middle;
+    }
+
+    InputDialog > Vertical {
+        background: $panel;
+        height: auto;
+        width: auto;
+        border: thick $primary;
+    }
+
+    InputDialog > Vertical > * {
+        width: auto;
+        height: auto;
+    }
+
+    InputDialog Input {
+        width: 40;
+        margin: 1;
+    }
+
+    InputDialog Label {
+        margin-left: 2;
+    }
+
+    InputDialog Button {
+        margin-right: 1;
+    }
+
+    InputDialog #buttons {
+        width: 100%;
+        align-horizontal: right;
+        padding-right: 1;
+    }
+    """
+    """The default styling for the input dialog."""
+
+    BINDINGS = [
+        Binding("escape", "app.pop_screen", "", show=False),
+    ]
+    """Bindings for the dialog."""
+
+    def __init__(
+            self,
+            prompt: str,
+            initial: str | None = None,
+            validator: Validator = None
+    ) -> None:
+        """Initialise the input dialog.
+
+        Args:
+            prompt: The prompt for the input.
+            initial: The initial value for the input.
+        """
+        super().__init__()
+        self._prompt = prompt
+        """The prompt to display for the input."""
+        self._initial = initial
+        """The initial value to use for the input."""
+        self._validator = validator or Function(function=lambda s: True)
+
+    def compose(self) -> ComposeResult:
+        """Compose the child widgets."""
+        with Vertical():
+            with Vertical(id="input"):
+                yield Label(self._prompt)
+                yield Input(
+                    self._initial or "",
+                    validators=[self._validator],
+                )
+            with Horizontal(id="buttons"):
+                yield Button("OK", id="ok", variant="primary")
+                yield Button("Cancel", id="cancel")
+
+    def on_mount(self) -> None:
+        """Set up the dialog once the DOM is ready."""
+        self.query_one(Input).focus()
+
+    @on(Button.Pressed, "#cancel")
+    def cancel_input(self) -> None:
+        """Cancel the input operation."""
+        self.dismiss()
+
+    @on(Input.Submitted)
+    @on(Button.Pressed, "#ok")
+    def accept_input(self) -> None:
+        """Accept and return the input."""
+        if ((value := self.query_one(Input).value.strip())
+                and self._validator.validate(value).is_valid):
+            self.dismiss(value)
+        else:
+            self.dismiss()
 
 
 class InteractiveLogMergeViewerApp(App):
@@ -16,6 +120,7 @@ class InteractiveLogMergeViewerApp(App):
 
     BINDINGS = [
         Binding(key="q", action="quit", description="Quit"),
+        Binding(key="l", action="goto_line", description="Go to line"),
     ]
 
     def __init__(self, *args, **kwargs):
@@ -96,3 +201,17 @@ class InteractiveLogMergeViewerApp(App):
                     if self.show_line_numbers else row_values[0],
                     *row_values[1:],
                     height=max_line_count(row_values))
+
+    # methods to support go to line function
+    def action_goto_line(self):
+        self.app.push_screen(
+            ModalInputDialog("Go to line:", validator=Integer(minimum=1)),
+            self.move_to_line
+        )
+
+    def move_to_line(self, line_str):
+        # convert 1-based line number to 0-based
+        line = int(line_str) - 1
+
+        dt_widget: DataTable = self.query_one(DataTable)
+        dt_widget.move_cursor(row=line, animate=False)
